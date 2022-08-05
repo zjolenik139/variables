4657ca933b60db788ea819c87d4b8261710470b2
Zachariah Olenik <zjolenik139@gmail.com>
2022-08-05 14:28:15 -0400

    variables program

---
+++ Variables.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace veriables_exersice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String firstName = "Zachariah";
+            int myAge = 26;
+            char myCharacter = 'Z';
+            bool isBool = true;
+            double myDouble = 26.2;
+            decimal myDecimal = 26.2m;
+
+         Console.WriteLine($"My name is {firstName}. My name age is {myAge} My first letter is {myCharacter}. This is {isBool}. {myDouble} {myDecimal}");
+        }
+    }
+}
